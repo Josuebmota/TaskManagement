aaf3baa08d32c6cd569b7efcfe9ba28811ed01a0
Josuebmota <josuebatistam1@gmail.com>
2020-04-26 21:29:04 -0300

    Criando estrutura base

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const routes = require ('./routes');
+
+import './database';
+
+class App {
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
